1f3ab0225069d4a501566750dc4e91052fd0a3fb
islomnumanov <islomnumanov.uz@gmail.com>
2022-11-26 21:39:01 +0300

    feat: Add component and initial game structure

--- tic-tac-toe/src/Game.tsx
+++ tic-tac-toe/src/Game.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import {useState} from "react";
+import Square from "./components/Square/Square";
 
 function Game() {
-
+  const INITIAL_GAME_STATE = ["X","O","","","","","","","",]
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE)
   return (
     <section className="min-h-screen flex flex-col w-4/5 m-auto">
       <header className="header">
-        <nav>
+        <nav className="text-center text-3xl">
           Hey I'm navbar
         </nav>
-        <ul className="flex justify-between">
+        <ul className="flex justify-evenly ">
           <li>Board goes here</li>
           <li>Score goes here</li>
         </ul>
       </header>
       <main className="main flex-auto p-8 text-slate-800">
-        <h1 className="text-center">Hello</h1>
-        <p>hey</p>
+        <h1 className="text-center text-6xl">Hello</h1>
+        <div className="grid grid-cols-3 gap-3 mx-auto w-96">
+          {
+            gameState.map((player, index) => {
+             return (
+              <Square key={index}>
+                {player}
+              </Square>
+             )
+            })
+          }
+        </div>
       </main>
       <footer className="footer">
         <p className="text-center">Made by Islom Numanov</p>
