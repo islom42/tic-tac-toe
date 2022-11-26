d41b2afc7e0a41bdb0e9770450c31c37c28b75ca
islomnumanov <islomnumanov.uz@gmail.com>
2022-11-26 21:23:42 +0300

    feat: Adding styles

--- tic-tac-toe/src/Game.tsx
+++ tic-tac-toe/src/Game.tsx
@@ -3,19 +3,24 @@ import React from "react";
 function Game() {
 
   return (
-    <React.Fragment>
-      <header>
+    <section className="min-h-screen flex flex-col w-4/5 m-auto">
+      <header className="header">
         <nav>
           Hey I'm navbar
         </nav>
+        <ul className="flex justify-between">
+          <li>Board goes here</li>
+          <li>Score goes here</li>
+        </ul>
       </header>
-      <main className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
+      <main className="main flex-auto p-8 text-slate-800">
         <h1 className="text-center">Hello</h1>
+        <p>hey</p>
       </main>
-      <footer>
+      <footer className="footer">
         <p className="text-center">Made by Islom Numanov</p>
       </footer>
-    </React.Fragment>
+    </section>
   )
 }
 
